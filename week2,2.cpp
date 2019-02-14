05f3f8a1c046aa960dfb0ef54d66c7aad2895bbd
vedaantsharma <43724969+vedaantsharma@users.noreply.github.com>
2019-02-14 12:37:00 +0530

    Add files via upload

---
+++ week2,2.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int n , first=0,second= 1,next,i;
+	printf("enter number of terms:");
+	scanf("%d",&n);
+	for(i=0;i<=n;i++)
+	{
+		if(i<=1)
+		
+			next=i;
+		else{
+			next= first+second;
+			first=second;
+			second=next;
+			
+		}
+		printf("%d\n",next);
+	}
+	return 0;
+}
